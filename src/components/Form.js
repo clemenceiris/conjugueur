d5197a1114b87154568dd660ccc87bd4516b1375
GAZIER CLEMENCE <CGAZIER@editions-hatier.fr>
2021-05-27 15:31:32 +0200

    add filter

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Downshift from 'downshift';
 import {all as starWarsNames} from 'starwars-names'
+import {matchSorter} from 'match-sorter'
+import { useState } from 'react';
+
 
 const items = starWarsNames.map(name => ({
     value: name,
     id: name.toLowerCase(),
 }))
+
+const getItems = value => value ? matchSorter(items, value, {keys: ['value']}) : items
 
 const itemToString = item => item ? item.value : ''
 
@@ -16,13 +21,31 @@ class Form extends React.Component {
                 <h1>Autocomplete Rocks</h1>
                 <div>
                     <Downshift itemToString={itemToString}>
-                        {({getLabelProps, getInputProps, getMenuProps, isOpen, selectItem}) => (
+                        {({
+                            getLabelProps, 
+                            getInputProps, 
+                            getMenuProps, 
+                            getItemProps,
+                            getToggleButtonProps,
+                            highlightedIndex,
+                            selectedItem,
+                            clearItem,
+                            isOpen, 
+                            inputValue,
+                        }) => (
                             <div>
                                 <label {...getLabelProps()} >Star Wars Character</label>
                                 <input {...getInputProps()} />
+                                <button {...getToggleButtonProps()}>
+                                    {isOpen ? 'close' : 'open'}
+                                </button>
+                                {selectedItem ? <button onClick={clearItem}>X</button> : null }
                                 <ul {...getMenuProps()}>
-                                    {isOpen ? items.map(item => (
-                                        <li key={item.id} onClick={() => selectItem(item)}>{item.value}</li>
+                                    {isOpen 
+                                    ? getItems(inputValue).map((item, index) => (
+                                        <li key={item.id} {...getItemProps({item, key:item.id, style: {
+                                            backgroundColor: index === highlightedIndex ? 'lightgray' : null,
+                                        }})}>{item.value}</li>
                                     )) : null}
                                 </ul>
                             </div>
